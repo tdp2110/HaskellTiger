4cc71ef75149d1872555d561c95da2f4a3cf1bb2
Tom Peters <thomas.d.peters@gmail.com>
2019-03-28 21:44:13 -0400

    getting started with haskell

--- chapter1/bla.hs
+++ chapter1/bla.hs
@@ -10,6 +10,19 @@ data Exp = IdExp Id
          | OpExp Exp BinOp Exp
          | EseqExp Stm Exp deriving Show
 
+-- problem 1
+maxargs :: Stm -> Int
+maxargs (CompountStm stm1 stm2) = max (maxargs stm1) (maxargs stm2)
+maxargs (AssignStm _ expr) = maxargsExp expr
+maxargs (PrintStm exprs) = max (length exprs) (maximum $ map maxargsExp exprs)
+
+maxargsExp :: Exp -> Int
+maxargsExp (OpExp expr1 _ expr2) = max (maxargsExp expr1) (maxargsExp expr2)
+maxargsExp (EseqExp stm expr) = max (maxargs stm) (maxargsExp expr)
+maxargsExp _ = 0
+
+prog1 :: Exp
+prog2 :: Stm
 prog1 = OpExp (NumExp 1337) Minus (IdExp "var")
 prog2 = CompountStm (
   AssignStm "a" (OpExp (NumExp 5) Plus (NumExp 3)))
@@ -20,3 +33,4 @@ main = do
   putStrLn "hello world"
   putStrLn $ show prog1
   putStrLn $ show prog2
+  putStrLn $ show $ maxargs prog2
