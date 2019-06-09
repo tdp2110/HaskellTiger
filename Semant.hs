e3190148b5ae62d76d928d87be254ce482ed9d74
Tom Peters <thomas.d.peters@gmail.com>
2019-06-09 09:34:22 -0400

    monadify transExp seqexp

--- Semant.hs
+++ Semant.hs
@@ -289,6 +289,14 @@ transExp' (A.SeqExp expAndPosns) = do
   case expTys of
     [] -> return ExpTy{exp=emptyExp, ty=Types.UNIT}
     _ -> return $ last expTys
+transExp' (A.AssignExp var expr pos) = do
+  ExpTy{exp=_, ty=varTy} <- transVar var
+  ExpTy{exp=_, ty=exprTy} <- transExp' expr
+  if varTy == exprTy then
+    return ExpTy{exp=emptyExp, ty=Types.UNIT}
+    else
+    throwT pos ("in assignExp, variable has type " ++ (show varTy) ++
+                " but assign target has type " ++ (show exprTy))
 
 transExp state (A.VarExp var) =
   let
