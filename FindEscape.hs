1621b4a9f17065dddcd6a79b5bbba116badc53b4
Tom Peters <thomas.d.peters@gmail.com>
2019-06-22 20:50:52 -0400

    ready to find escapes

--- FindEscape.hs
+++ FindEscape.hs
@@ -1,8 +1,14 @@
-module FindEscape where
+module FindEscape (escapeExp) where
 
 import qualified Absyn as A
+import Symbol
+
+import Data.Map (Map)
+import Data.List
 import Prelude hiding (exp)
 
+
+escapeExp :: A.Exp -> A.Exp
 
 type AstPath = [AstDir]
 
@@ -30,56 +36,68 @@ data AstDir =
   | FunParam Int
   deriving (Show)
 
-escapeExp :: A.Exp -> AstPath -> A.Exp
-escapeExp callExp@(A.CallExp _ args _) (CallArg(idx):path) =
-  callExp{A.args=replaceNth idx args (escapeExp (args !! idx) path)}
-escapeExp opExp@(A.OpExp leftExp _ _ _) (OpLeft:path) =
-  opExp{A.left=escapeExp leftExp path}
-escapeExp opExp@(A.OpExp _ _ rightExp _) (OpRight:path) =
-  opExp{A.right=escapeExp rightExp path}
-escapeExp recordExp@(A.RecordExp fields _ _) (RecField(idx):path) =
+data EnvEntry = EnvEntry{depth :: Int, path :: AstPath}
+type Env = Map Symbol EnvEntry
+
+findEscapes :: A.Exp -> [AstPath]
+findEscapes _ = undefined
+
+escapePaths :: A.Exp -> [AstPath] -> A.Exp
+escapePaths exp paths =
+  foldl' escapeExpPath exp paths
+
+escapeExp exp = escapePaths exp (findEscapes exp)
+
+escapeExpPath :: A.Exp -> AstPath -> A.Exp
+escapeExpPath callExp@(A.CallExp _ args _) (CallArg(idx):path') =
+  callExp{A.args=replaceNth idx args (escapeExpPath (args !! idx) path')}
+escapeExpPath opExp@(A.OpExp leftExp _ _ _) (OpLeft:path') =
+  opExp{A.left=escapeExpPath leftExp path'}
+escapeExpPath opExp@(A.OpExp _ _ rightExp _) (OpRight:path') =
+  opExp{A.right=escapeExpPath rightExp path'}
+escapeExpPath recordExp@(A.RecordExp fields _ _) (RecField(idx):path') =
   let
     (sym, exp, pos) = fields !! idx
-    exp' = escapeExp exp path
+    exp' = escapeExpPath exp path'
   in
     recordExp{A.fields=replaceNth idx fields (sym, exp', pos)}
-escapeExp (A.SeqExp seqElts) (SeqElt(idx):path) =
+escapeExpPath (A.SeqExp seqElts) (SeqElt(idx):path') =
   let
     (exp, pos) = seqElts !! idx
-    exp' = escapeExp exp path
+    exp' = escapeExpPath exp path'
   in
     A.SeqExp $ replaceNth idx seqElts (exp', pos)
-escapeExp assignExp@(A.AssignExp _ exp _) (AssignExp:path) =
-  assignExp{A.exp=escapeExp exp path}
-escapeExp ifExp@(A.IfExp testExp _ _ _) (IfTest:path) =
-  ifExp{A.test=escapeExp testExp path}
-escapeExp ifExp@(A.IfExp _ thenExp _ _) (IfThen:path) =
-  ifExp{A.then'=escapeExp thenExp path}
-escapeExp ifExp@(A.IfExp _ _ (Just elseExp) _) (IfElse:path) =
-  ifExp{A.else'=Just $ escapeExp elseExp path}
-escapeExp whileExp@(A.WhileExp testExp _ _) (WhileTest:path) =
-  whileExp{A.test=escapeExp testExp path}
-escapeExp whileExp@(A.WhileExp _ bodyExp _) (WhileBody:path) =
-  whileExp{A.body=escapeExp bodyExp path}
-escapeExp forExp@(A.ForExp _ _ _ _ _ _) [] = forExp{A.escape=True}
-escapeExp forExp@(A.ForExp _ _ loExp _ _ _) (ForLo:path) =
-  forExp{A.lo=escapeExp loExp path}
-escapeExp forExp@(A.ForExp _ _ _ hiExp _ _) (ForHi:path) =
-  forExp{A.hi=escapeExp hiExp path}
-escapeExp forExp@(A.ForExp _ _ _ _ bodyExp _) (ForBody:path) =
-  forExp{A.body=escapeExp bodyExp path}
-escapeExp letExp@(A.LetExp decs _ _) (LetDec(idx):path) =
-  letExp{A.decs=replaceNth idx decs (escapeDec (decs !! idx) path)}
-escapeExp letExp@(A.LetExp _ bodyExp _) (LetBody:path) =
-  letExp{A.body=escapeExp bodyExp path}
-escapeExp arrayExp@(A.ArrayExp _ sizeExp _ _) (ArraySize:path) =
-  arrayExp{A.size=escapeExp sizeExp path}
-escapeExp arrayExp@(A.ArrayExp _ _ initExp _) (ArrayInit:path) =
-  arrayExp{A.init=escapeExp initExp path}
-escapeExp _ _ = error "shouldn't get here"
+escapeExpPath assignExp@(A.AssignExp _ exp _) (AssignExp:path') =
+  assignExp{A.exp=escapeExpPath exp path'}
+escapeExpPath ifExp@(A.IfExp testExp _ _ _) (IfTest:path') =
+  ifExp{A.test=escapeExpPath testExp path'}
+escapeExpPath ifExp@(A.IfExp _ thenExp _ _) (IfThen:path') =
+  ifExp{A.then'=escapeExpPath thenExp path'}
+escapeExpPath ifExp@(A.IfExp _ _ (Just elseExp) _) (IfElse:path') =
+  ifExp{A.else'=Just $ escapeExpPath elseExp path'}
+escapeExpPath whileExp@(A.WhileExp testExp _ _) (WhileTest:path') =
+  whileExp{A.test=escapeExpPath testExp path'}
+escapeExpPath whileExp@(A.WhileExp _ bodyExp _) (WhileBody:path') =
+  whileExp{A.body=escapeExpPath bodyExp path'}
+escapeExpPath forExp@(A.ForExp _ _ _ _ _ _) [] = forExp{A.escape=True}
+escapeExpPath forExp@(A.ForExp _ _ loExp _ _ _) (ForLo:path') =
+  forExp{A.lo=escapeExpPath loExp path'}
+escapeExpPath forExp@(A.ForExp _ _ _ hiExp _ _) (ForHi:path') =
+  forExp{A.hi=escapeExpPath hiExp path'}
+escapeExpPath forExp@(A.ForExp _ _ _ _ bodyExp _) (ForBody:path') =
+  forExp{A.body=escapeExpPath bodyExp path'}
+escapeExpPath letExp@(A.LetExp decs _ _) (LetDec(idx):path') =
+  letExp{A.decs=replaceNth idx decs (escapeDecPath (decs !! idx) path')}
+escapeExpPath letExp@(A.LetExp _ bodyExp _) (LetBody:path') =
+  letExp{A.body=escapeExpPath bodyExp path'}
+escapeExpPath arrayExp@(A.ArrayExp _ sizeExp _ _) (ArraySize:path') =
+  arrayExp{A.size=escapeExpPath sizeExp path'}
+escapeExpPath arrayExp@(A.ArrayExp _ _ initExp _) (ArrayInit:path') =
+  arrayExp{A.init=escapeExpPath initExp path'}
+escapeExpPath _ _ = error "shouldn't get here"
 
-escapeDec :: A.Dec -> AstPath -> A.Dec
-escapeDec (A.FunctionDec funDecs) [FunDec(funIdx), FunParam(paramIdx)] =
+escapeDecPath :: A.Dec -> AstPath -> A.Dec
+escapeDecPath (A.FunctionDec funDecs) [FunDec(funIdx), FunParam(paramIdx)] =
   A.FunctionDec $ replaceNth funIdx funDecs (escapeParam (funDecs !! funIdx) paramIdx)
   where
     escapeParam :: A.FunDec -> Int -> A.FunDec
@@ -87,8 +105,8 @@ escapeDec (A.FunctionDec funDecs) [FunDec(funIdx), FunParam(paramIdx)] =
       funDec{A.params=replaceNth idx params (escapeField $ params !! idx)}
     escapeField :: A.Field -> A.Field
     escapeField field@(A.Field _ _ _ _) = field{A.fieldEscape=True}
-escapeDec varDec@(A.VarDec _ _ _ _ _) [] = varDec{A.vardecEscape=True}
-escapeDec _ _ = error "shouldn't get here"
+escapeDecPath varDec@(A.VarDec _ _ _ _ _) [] = varDec{A.vardecEscape=True}
+escapeDecPath _ _ = error "shouldn't get here"
 
 replaceNth :: Int -> [a] -> a -> [a]
 replaceNth idx xs replacement =
