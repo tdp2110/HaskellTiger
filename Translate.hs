c2ef0619966680de527a85bfafd8a8574cd960c7
Tom Peters <thomas.d.peters@gmail.com>
2019-07-07 09:58:41 -0400

    working on Translate.hs

--- Translate.hs
+++ Translate.hs
@@ -1,3 +1,18 @@
+{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE AllowAmbiguousTypes #-}
+
 module Translate where
 
+import qualified Temp
+import qualified Frame
+
 newtype Exp = Exp () deriving (Show)
+
+class Translator f where
+  type Level f :: *
+  type Access f :: *
+  outermost :: Level f
+  newLevel :: (Level f, Temp.Label, [Frame.EscapesOrNot]) -> Level f
+  formals :: Level f -> [Access f]
+  allocLocal :: Level f -> Temp.Generator -> Frame.EscapesOrNot
+    -> (Temp.Generator, Access f)
