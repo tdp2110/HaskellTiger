ee4913b4d1e56dbe4ddde581d1b9ec5aafb09ecf
Tom Peters <thomas.d.peters@gmail.com>
2019-09-14 22:12:17 -0400

    useless formatting

--- src/Absyn.hs
+++ src/Absyn.hs
@@ -2,9 +2,9 @@ module Absyn where
 
 import Symbol
 
-data Pos = Pos {absChrOffset :: !Int,
-                lineno :: !Int,
-                colno :: !Int}
+data Pos = Pos { absChrOffset :: !Int
+               , lineno :: !Int
+               , colno :: !Int}
   deriving (Eq)
 
 instance Show Pos where
@@ -19,49 +19,49 @@ data Exp = VarExp Var
          | NilExp
          | IntExp Int
          | StringExp [Char]
-         | CallExp {func :: Symbol,
-                    args :: [Exp],
-                    pos :: Pos}
-         | OpExp {left :: Exp,
-                  oper :: Oper,
-                  right :: Exp,
-                  pos :: Pos}
-         | RecordExp {fields :: [(Symbol, Exp, Pos)],
-                      typ :: Symbol,
-                      pos :: Pos}
+         | CallExp { func :: Symbol
+                   , args :: [Exp]
+                   , pos :: Pos }
+         | OpExp { left :: Exp
+                 , oper :: Oper
+                 , right :: Exp
+                 , pos :: Pos }
+         | RecordExp { fields :: [(Symbol, Exp, Pos)]
+                     , typ :: Symbol
+                     , pos :: Pos }
          | SeqExp [(Exp, Pos)]
-         | AssignExp {var :: Var,
-                      exp :: Exp,
-                      pos :: Pos}
-         | IfExp {test :: Exp,
-                  then' :: Exp,
-                  else' :: Maybe Exp,
-                  pos :: Pos}
-         | WhileExp {test :: Exp,
-                     body :: Exp,
-                     pos :: Pos}
-         | ForExp {forVar :: Symbol,
-                   escape :: Bool,
-                   lo :: Exp,
-                   hi :: Exp,
-                   body :: Exp,
-                   pos :: Pos}
+         | AssignExp { var :: Var
+                     , exp :: Exp
+                     , pos :: Pos }
+         | IfExp { test :: Exp
+                 , then' :: Exp
+                 , else' :: Maybe Exp
+                 , pos :: Pos }
+         | WhileExp { test :: Exp
+                    , body :: Exp
+                    , pos :: Pos }
+         | ForExp { forVar :: Symbol
+                  , escape :: Bool
+                  , lo :: Exp
+                  , hi :: Exp
+                  , body :: Exp
+                  , pos :: Pos }
          | BreakExp Pos
-         | LetExp {decs :: [Dec],
-                   body :: Exp,
-                   pos ::Pos}
-         | ArrayExp {typ :: Symbol,
-                     size :: Exp,
-                     init :: Exp,
-                     pos :: Pos}
+         | LetExp { decs :: [Dec]
+                  , body :: Exp
+                  , pos ::Pos }
+         | ArrayExp { typ :: Symbol
+                    , size :: Exp
+                    , init :: Exp
+                    , pos :: Pos }
          deriving (Eq, Show)
 
 data Dec = FunctionDec [FunDec]
-         | VarDec {name :: Symbol,
-                   vardecEscape :: Bool,
-                   varDecTyp :: Maybe (Symbol, Pos),
-                   decInit :: Exp,
-                   decPos :: Pos}
+         | VarDec { name :: Symbol
+                  , vardecEscape :: Bool
+                  , varDecTyp :: Maybe (Symbol, Pos)
+                  , decInit :: Exp
+                  , decPos :: Pos }
          | TypeDec [TyDec]
          deriving (Eq, Show)
 
@@ -74,20 +74,20 @@ data Oper = PlusOp | MinusOp | TimesOp | DivideOp
           | EqOp | NeqOp | LtOp | LeOp | GtOp | GeOp
           deriving (Eq, Show)
 
-data Field = Field {fieldName :: Symbol,
-                    fieldEscape :: Bool,
-                    fieldTyp :: Symbol,
-                    fieldPos :: Pos}
+data Field = Field { fieldName :: Symbol
+                   , fieldEscape :: Bool
+                   , fieldTyp :: Symbol
+                   , fieldPos :: Pos }
            deriving (Eq, Show)
 
-data FunDec = FunDec {fundecName :: Symbol,
-                      params :: [Field],
-                      result :: Maybe (Symbol, Pos),
-                      funBody :: Exp,
-                      funPos :: Pos}
+data FunDec = FunDec { fundecName :: Symbol
+                     , params :: [Field]
+                     , result :: Maybe (Symbol, Pos)
+                     , funBody :: Exp
+                     , funPos :: Pos }
             deriving (Eq, Show)
 
-data TyDec = TyDec {tydecName :: Symbol,
-                    ty :: Ty,
-                    tydecPos :: Pos}
+data TyDec = TyDec { tydecName :: Symbol
+                   , ty :: Ty
+                   , tydecPos :: Pos }
             deriving (Eq, Show)
