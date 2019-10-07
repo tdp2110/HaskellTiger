be61fa92d3e4e817adec47a6d35c2b65c926ac35
Tom Peters <thomas.d.peters@gmail.com>
2019-10-06 23:04:01 -0400

    adding in appel's comments

--- src/Canon.hs
+++ src/Canon.hs
@@ -130,6 +130,17 @@ linearize s gen =
 
 type Block = [T.Stm]
 
+{-
+From a list of cleaned trees, produce a list of
+         basic blocks satisfying the following properties:
+              1. and 2. as above;
+              3.  Every block begins with a LABEL;
+              4.  A LABEL appears only at the beginning of a block;
+              5.  Any JUMP or CJUMP is the last stm in a block;
+              6.  Every block ends with a JUMP or CJUMP;
+           Also produce the "label" to which control will be passed
+           upon exit.
+-}
 basicBlocks :: [T.Stm] -> State Temp.Generator ([Block], Temp.Label)
 basicBlocks stms = do
   done <- newLabel
@@ -233,6 +244,16 @@ getNext _ [] = do
   pure []
 getNext _ _ = impossible
 
+{-
+From a list of basic blocks satisfying properties 1-6,
+            along with an "exit" label,
+            produce a list of stms such that:
+              1. and 2. as above;
+              7. Every CJUMP(_,t,f) is immediately followed by LABEL f.
+            The blocks are reordered to satisfy property 7; also
+            in this reordering as many JUMP(T.NAME(lab)) statements
+            as possible are eliminated by falling through into T.LABEL(lab).
+-}
 traceSchedule :: [Block] -> Temp.Label -> State Temp.Generator Block
 traceSchedule blocks done = do
   allExceptDone <- getNext (foldr' enterBlock Map.empty blocks) blocks
