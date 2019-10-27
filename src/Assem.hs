666fba69f9f859be5dfb7ce94cb5e59a1f41af94
Tom Peters <thomas.d.peters@gmail.com>
2019-10-27 14:52:37 -0400

    starting instruction selection

---
+++ src/Assem.hs
@@ -0,0 +1,15 @@
+module Assem where
+
+import qualified Temp
+
+type Label = Temp.Label
+
+data Inst = OPER { assem :: String
+                 , operDst :: [Int]
+                 , operSrc :: [Int]
+                 , jump :: [Label] }
+          | LABEL { assem :: String
+                  , lab :: Label }
+          | MOVE { assem :: String
+                 , moveDst :: Int
+                 , moveSrc :: Int }
---
+++ src/PrintAssem.hs
@@ -0,0 +1,8 @@
+module Main where
+
+import qualified Assem
+
+
+main :: IO ()
+main = do
+  return ()
