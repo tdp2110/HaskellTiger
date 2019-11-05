a399b8d4d4df4640c34b450ea477025ceef57448
Tom Peters <thomas.d.peters@gmail.com>
2019-11-04 22:58:16 -0500

    move printStm/Exp logic into Tree.hs and out of IO ()

--- src/PrintTree.hs
+++ src/PrintTree.hs
@@ -2,134 +2,9 @@ module Main where
 
 import qualified Parser
 import qualified Semant
-import qualified Symbol
-import qualified Temp
 import qualified Translate
-import qualified Tree as T
 
 import System.Environment (getArgs)
-import Prelude hiding (exp)
-
-indent :: Int -> IO ()
-indent 0 = do
-  pure ()
-indent i = do
-  putStr "  "
-  indent $ i - 1
-
-stm :: T.Stm -> Int -> IO ()
-stm (T.SEQ (a,b)) d = do
-  indent d
-  putStrLn "SEQ("
-  stm a $ d + 1
-  putStrLn ","
-  stm b $ d + 1
-  putChar ')'
-stm (T.LABEL (Temp.Label(Symbol.Symbol lab))) d = do
-  indent d
-  putStr "LABEL "
-  putStr lab
-stm (T.JUMP (e,_)) d = do
-  indent d
-  putStrLn "JUMP("
-  exp e $ d + 1
-  putChar ')'
-stm (T.CJUMP (r,a,b,Temp.Label(Symbol.Symbol t),Temp.Label (Symbol.Symbol f))) d = do
-  indent d
-  putStr "CJUMP("
-  relop r
-  putStrLn ","
-  exp a $ d + 1
-  putStrLn ","
-  exp b $ d + 1
-  putStrLn ", "
-  indent $ d + 1
-  putStr t
-  putChar ','
-  putStr f
-  putChar ')'
-stm (T.MOVE (a,b)) d = do
-  indent d
-  putStrLn "MOVE"
-  exp a $ d + 1
-  putStrLn ","
-  exp b $ d + 1
-  putChar ')'
-stm (T.EXP e) d = do
-  indent d
-  putStrLn "EXP("
-  exp e $ d + 1
-  putChar ')'
-
-exp :: T.Exp -> Int -> IO ()
-exp (T.BINOP (p,a,b)) d = do
-  indent d
-  putStr "BINOP("
-  binop p
-  putStrLn ","
-  exp a $ d + 1
-  putStrLn ","
-  exp b $ d + 1
-  putChar ')'
-exp (T.MEM e) d = do
-  indent d
-  putStrLn "MEM("
-  exp e $ d + 1
-  putChar ')'
-exp (T.TEMP t) d = do
-  indent d
-  putStr "TEMP "
-  putStr $ show t
-exp (T.ESEQ (s,e)) d = do
-  indent d
-  putStrLn "ESEQ("
-  stm s $ d + 1
-  putStrLn ","
-  exp e $ d + 1
-  putChar ')'
-exp (T.NAME (Temp.Label (Symbol.Symbol lab))) d = do
-  indent d
-  putStr "NAME "
-  putStr lab
-exp (T.CONST i) d = do
-  indent d
-  putStr "CONST "
-  putStr $ show i
-exp (T.CALL (e,el)) d = do
-  indent d
-  putStrLn "CALL("
-  exp e $ d + 1
-  mapM_
-    (\a -> do
-              putStrLn ","
-              exp a $ d + 2
-    )
-    el
-  putStr ")"
-
-binop :: T.Binop -> IO ()
-binop T.PLUS = do putStr "PLUS"
-binop T.MINUS = do putStr "MINUS"
-binop T.MUL = do putStr "MUL"
-binop T.DIV = do putStr "DIV"
-binop T.AND = do putStr "AND"
-binop T.OR = do putStr "OR"
-binop T.LSHIFT = do putStr "LSHIFT"
-binop T.RSHIFT = do putStr "RSHIFT"
-binop T.ARSHIFT = do putStr "ARSHIFT"
-binop T.XOR = do putStr "XOR"
-
-relop :: T.Relop -> IO ()
-relop T.EQ = do putStr "EQ"
-relop T.NE = do putStr "NE"
-relop T.LT = do putStr "LT"
-relop T.GT = do putStr "GT"
-relop T.LE = do putStr "LE"
-relop T.GE = do putStr "GE"
-relop T.ULT = do putStr "ULT"
-relop T.ULE = do putStr "ULE"
-relop T.UGT = do putStr "UGT"
-relop T.UGE = do putStr "UGE"
 
 parseToExp :: String -> Translate.Exp
 parseToExp text =
@@ -151,6 +26,6 @@ main = do
   args <- getArgs
   str <- readFile $ head args
   case parseToExp str of
-    Translate.Ex treeExp -> exp treeExp 0 >> putStrLn ""
-    Translate.Nx treeStm -> stm treeStm 0 >> putStrLn ""
+    Translate.Ex treeExp -> putStrLn $ show treeExp
+    Translate.Nx treeStm -> putStrLn $ show treeStm
     Translate.Cx _ -> do putStrLn "CX ..."
--- src/Tree.hs
+++ src/Tree.hs
@@ -1,8 +1,13 @@
 module Tree where
 
+import qualified Symbol
 import qualified Temp
 
+import Control.Monad.Trans.Writer (Writer, tell, execWriter)
+import Data.DList (DList, singleton, toList, fromList)
+
 import Prelude hiding (GT, LT, EQ)
+
 
 data Exp =
     CONST Int
@@ -12,7 +17,7 @@ data Exp =
   | MEM Exp
   | CALL (Exp, [Exp])
   | ESEQ (Stm, Exp)
-  deriving (Eq, Show)
+  deriving (Eq)
 
 data Stm =
     MOVE (Exp, Exp)
@@ -21,7 +26,13 @@ data Stm =
   | CJUMP (Relop, Exp, Exp, Temp.Label, Temp.Label)
   | SEQ (Stm, Stm)
   | LABEL Temp.Label
-  deriving (Eq, Show)
+  deriving (Eq)
+
+instance Show Stm where
+  show stm = toList $ execWriter $ putStm stm 0
+
+instance Show Exp where
+  show expr = toList $ execWriter $ putExp expr 0
 
 data Binop =
     PLUS
@@ -36,7 +47,7 @@ data Binop =
   | XOR
   deriving (Eq, Show)
 
-data Relop =
+data  Relop =
     EQ
   | NE
   | LT
@@ -62,3 +73,138 @@ notRel op =
     UGT -> ULE
     ULE -> UGT
     UGE -> ULT
+
+type StmWriter = Writer (DList Char) ()
+
+putStrW :: String -> StmWriter
+putStrW s = do
+              tell $ fromList s
+              pure ()
+
+putStrLnW :: String -> StmWriter
+putStrLnW s = putStrW $ s ++ "\n"
+
+putCharW :: Char -> StmWriter
+putCharW c = tell $ singleton c
+
+indent :: Int -> StmWriter
+indent i
+  | i == 0    = do pure ()
+  | otherwise = do
+                  putStrW "  "
+                  indent $ i - 1
+
+
+putStm :: Stm -> Int -> StmWriter
+putStm (SEQ (a,b)) d = do
+  indent d
+  putStrLnW "SEQ("
+  putStm a $ d + 1
+  putStrLnW ","
+  putStm b $ d + 1
+  putCharW ')'
+putStm (LABEL (Temp.Label(Symbol.Symbol lab))) d = do
+  indent d
+  putStrW "LABEL "
+  putStrW lab
+putStm (JUMP (e,_)) d = do
+  indent d
+  putStrLnW "JUMP("
+  putExp e $ d + 1
+  putCharW ')'
+putStm (CJUMP (r,a,b,Temp.Label(Symbol.Symbol t),Temp.Label (Symbol.Symbol f))) d = do
+  indent d
+  putStrW "CJUMP("
+  relop r
+  putStrLnW ","
+  putExp a $ d + 1
+  putStrLnW ","
+  putExp b $ d + 1
+  putStrLnW ", "
+  indent $ d + 1
+  putStrW t
+  putCharW ','
+  putStrW f
+  putCharW ')'
+putStm (MOVE (a,b)) d = do
+  indent d
+  putStrLnW "MOVE"
+  putExp a $ d + 1
+  putStrLnW ","
+  putExp b $ d + 1
+  putCharW ')'
+putStm (EXP e) d = do
+  indent d
+  putStrLnW "EXP("
+  putExp e $ d + 1
+  putCharW ')'
+
+putExp :: Exp -> Int -> StmWriter
+putExp (BINOP (p,a,b)) d = do
+  indent d
+  putStrW "BINOP("
+  binop p
+  putStrLnW ","
+  putExp a $ d + 1
+  putStrLnW ","
+  putExp b $ d + 1
+  putCharW ')'
+putExp (MEM e) d = do
+  indent d
+  putStrLnW "MEM("
+  putExp e $ d + 1
+  putCharW ')'
+putExp (TEMP t) d = do
+  indent d
+  putStrW "TEMP "
+  putStrW $ show t
+putExp (ESEQ (s,e)) d = do
+  indent d
+  putStrLnW "ESEQ("
+  putStm s $ d + 1
+  putStrLnW ","
+  putExp e $ d + 1
+  putCharW ')'
+putExp (NAME (Temp.Label (Symbol.Symbol lab))) d = do
+  indent d
+  putStrW "NAME "
+  putStrW lab
+putExp (CONST i) d = do
+  indent d
+  putStrW "CONST "
+  putStrW $ show i
+putExp (CALL (e,el)) d = do
+  indent d
+  putStrLnW "CALL("
+  putExp e $ d + 1
+  mapM_
+    (\a -> do
+             putStrLnW ","
+             putExp a $ d + 2
+    )
+    el
+  putStrW ")"
+
+binop :: Binop -> StmWriter
+binop PLUS = do putStrW "PLUS"
+binop MINUS = do putStrW "MINUS"
+binop MUL = do putStrW "MUL"
+binop DIV = do putStrW "DIV"
+binop AND = do putStrW "AND"
+binop OR = do putStrW "OR"
+binop LSHIFT = do putStrW "LSHIFT"
+binop RSHIFT = do putStrW "RSHIFT"
+binop ARSHIFT = do putStrW "ARSHIFT"
+binop XOR = do putStrW "XOR"
+
+relop :: Relop -> StmWriter
+relop EQ = do putStrW "EQ"
+relop NE = do putStrW "NE"
+relop LT = do putStrW "LT"
+relop GT = do putStrW "GT"
+relop LE = do putStrW "LE"
+relop GE = do putStrW "GE"
+relop ULT = do putStrW "ULT"
+relop ULE = do putStrW "ULE"
+relop UGT = do putStrW "UGT"
+relop UGE = do putStrW "UGE"
