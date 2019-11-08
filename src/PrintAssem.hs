83d00f3d7d9a1a75de4e1f8d1b6de81465f15b7f
Tom Peters <thomas.d.peters@gmail.com>
2019-11-07 23:03:00 -0500

    start formatting asm

--- src/PrintAssem.hs
+++ src/PrintAssem.hs
@@ -12,7 +12,14 @@ import qualified X64Frame
 import Control.Monad (join)
 import Control.Monad.Trans.State (runState)
 import Data.List
+import qualified Data.Map as Map
 import System.Environment (getArgs)
+
+printTemp :: X64Frame.X64 -> Int -> String
+printTemp x64 reg =
+  case Map.lookup reg $ X64Frame.tempMap x64 of
+    Just s -> s
+    Nothing -> "t" ++ show reg
 
 compileToAsm :: String -> String
 compileToAsm text =
@@ -29,8 +36,45 @@ compileToAsm text =
                           step1
                           ([], gen4)
                           stmts'
+        formatAsm :: Assem.Inst -> String
+        formatAsm asm =
+          let
+            formatImpl :: String -> [Int] -> [Int] -> Maybe [Assem.Label] -> String
+            formatImpl assem dsts srcs jmps =
+              let
+                readInt :: Char -> Int
+                readInt c = read $ c : [] :: Int
+                getJmp :: Maybe [Assem.Label] -> Int -> String
+                getJmp (Just labs) idx =
+                  Temp.name $ labs !! idx
+                getJmp _ _ = error "shouldn't get here"
+                go ('`':'d':i:rest) = (printTemp x64 $ dsts !! readInt i) ++ go rest
+                go ('`':'s':i:rest) = (printTemp x64 $ srcs !! readInt i) ++ go rest
+                go ('`':'j':i:rest) = (getJmp jmps $ readInt i) ++ go rest
+                go (c:rest) = c : go rest
+                go [] = []
+              in
+                go assem
+          in
+            case asm of
+              Assem.OPER { Assem.assem=assem
+                         , Assem.operDst=operDst
+                         , Assem.operSrc=operSrc
+                         , Assem.jump=jmps } -> formatImpl
+                                                  assem
+                                                  operDst
+                                                  operSrc
+                                                  jmps
+              Assem.LABEL { Assem.assem=assem } -> assem
+              Assem.MOVE { Assem.assem=assem
+                         , Assem.moveDst=moveDst
+                         , Assem.moveSrc=moveSrc } -> formatImpl
+                                                        assem
+                                                        [moveDst]
+                                                        [moveSrc]
+                                                        Nothing
       in
-        ("PROC:\n" ++ join (map show insts), gen5)
+        ("PROC:\n" ++ join (fmap formatAsm insts), gen5)
       where
         step1 :: ([Assem.Inst], Temp.Generator) -> Tree.Stm -> ([Assem.Inst], Temp.Generator)
         step1 (insts, g) stm =
