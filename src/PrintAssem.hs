6b5fd38c460244710132e30c4315515c04676b2e
Tom Peters <thomas.d.peters@gmail.com>
2019-11-16 23:39:32 -0500

    show parse/semant errors in printAssem

--- src/PrintAssem.hs
+++ src/PrintAssem.hs
@@ -23,77 +23,81 @@ printTemp x64 reg =
 
 compileToAsm :: String -> String
 compileToAsm text =
-  let
-    (Right ast) = Parser.parse text
-    (Right (_, frags, gen, x64)) = Semant.transThunked ast
-    emit :: X64Frame.Frag -> Temp.Generator -> (String, Temp.Generator)
-    emit (X64Frame.PROC { X64Frame.body=bodyStm
-                        , X64Frame.fragFrame=frame }) gen' =
-      let
-        (stmts, gen'') = Canon.linearize bodyStm gen'
-        ((blocks, lab), gen3) = runState (Canon.basicBlocks stmts) gen''
-        (stmts', gen4) = runState (Canon.traceSchedule blocks lab) gen3
-        (insts, gen5) = foldl'
-                          step1
-                          ([], gen4)
-                          stmts'
-        insts' = X64Frame.procEntryExit3 frame insts
-        formatAsm :: Assem.Inst -> String
-        formatAsm asm =
+  case Parser.parse text of
+    Left err -> show err
+    Right ast ->
+      case Semant.transThunked ast of
+        Left err -> show err
+        (Right (_, frags, gen, x64)) ->
           let
-            formatImpl :: String -> [Int] -> [Int] -> Maybe [Assem.Label] -> String
-            formatImpl assem dsts srcs jmps =
+            emit :: X64Frame.Frag -> Temp.Generator -> (String, Temp.Generator)
+            emit (X64Frame.PROC { X64Frame.body=bodyStm
+                                , X64Frame.fragFrame=frame }) gen' =
               let
-                readInt :: Char -> Int
-                readInt c = read $ c : [] :: Int
-                getJmp :: Maybe [Assem.Label] -> Int -> String
-                getJmp (Just labs) idx =
-                  Temp.name $ labs !! idx
-                getJmp _ _ = error "shouldn't get here"
-                go ('`':'d':i:rest) = (printTemp x64 $ dsts !! readInt i) ++ go rest
-                go ('`':'s':i:rest) = (printTemp x64 $ srcs !! readInt i) ++ go rest
-                go ('`':'j':i:rest) = (getJmp jmps $ readInt i) ++ go rest
-                go (c:rest) = c : go rest
-                go [] = []
+                (stmts, gen'') = Canon.linearize bodyStm gen'
+                ((blocks, lab), gen3) = runState (Canon.basicBlocks stmts) gen''
+                (stmts', gen4) = runState (Canon.traceSchedule blocks lab) gen3
+                (insts, gen5) = foldl'
+                                  step1
+                                  ([], gen4)
+                                  stmts'
+                insts' = X64Frame.procEntryExit3 frame insts
+                formatAsm :: Assem.Inst -> String
+                formatAsm asm =
+                  let
+                    formatImpl :: String -> [Int] -> [Int] -> Maybe [Assem.Label] -> String
+                    formatImpl assem dsts srcs jmps =
+                      let
+                        readInt :: Char -> Int
+                        readInt c = read $ c : [] :: Int
+                        getJmp :: Maybe [Assem.Label] -> Int -> String
+                        getJmp (Just labs) idx =
+                          Temp.name $ labs !! idx
+                        getJmp _ _ = error "shouldn't get here"
+                        go ('`':'d':i:rest) = (printTemp x64 $ dsts !! readInt i) ++ go rest
+                        go ('`':'s':i:rest) = (printTemp x64 $ srcs !! readInt i) ++ go rest
+                        go ('`':'j':i:rest) = (getJmp jmps $ readInt i) ++ go rest
+                        go (c:rest) = c : go rest
+                        go [] = []
+                      in
+                        go assem
+                  in
+                    case asm of
+                      Assem.OPER { Assem.assem=assem
+                                 , Assem.operDst=operDst
+                                 , Assem.operSrc=operSrc
+                                 , Assem.jump=jmps } -> formatImpl
+                                                          assem
+                                                          operDst
+                                                          operSrc
+                                                          jmps
+                      Assem.LABEL { Assem.assem=assem } -> assem
+                      Assem.MOVE { Assem.assem=assem
+                                 , Assem.moveDst=moveDst
+                                 , Assem.moveSrc=moveSrc } -> formatImpl
+                                                                assem
+                                                                [moveDst]
+                                                                [moveSrc]
+                                                                Nothing
               in
-                go assem
+                (join (fmap formatAsm insts'), gen5)
+              where
+                step1 :: ([Assem.Inst], Temp.Generator) -> Tree.Stm -> ([Assem.Inst], Temp.Generator)
+                step1 (insts, g) stm =
+                  let
+                    (insts', g') = Codegen.codegen x64 g stm
+                  in
+                    (insts ++ insts', g')
+            emit (X64Frame.STRING (lab, str)) g =
+              ((Temp.name lab) ++ ":\n\t.asciz\t\"" ++ str ++ "\"\n", g)
+            step2 :: (String, Temp.Generator) -> X64Frame.Frag -> (String, Temp.Generator)
+            step2 (s, g) f =
+              let
+                (s', g') = emit f g
+              in
+                (s ++ s', g')
           in
-            case asm of
-              Assem.OPER { Assem.assem=assem
-                         , Assem.operDst=operDst
-                         , Assem.operSrc=operSrc
-                         , Assem.jump=jmps } -> formatImpl
-                                                  assem
-                                                  operDst
-                                                  operSrc
-                                                  jmps
-              Assem.LABEL { Assem.assem=assem } -> assem
-              Assem.MOVE { Assem.assem=assem
-                         , Assem.moveDst=moveDst
-                         , Assem.moveSrc=moveSrc } -> formatImpl
-                                                        assem
-                                                        [moveDst]
-                                                        [moveSrc]
-                                                        Nothing
-      in
-        (join (fmap formatAsm insts'), gen5)
-      where
-        step1 :: ([Assem.Inst], Temp.Generator) -> Tree.Stm -> ([Assem.Inst], Temp.Generator)
-        step1 (insts, g) stm =
-          let
-            (insts', g') = Codegen.codegen x64 g stm
-          in
-            (insts ++ insts', g')
-    emit (X64Frame.STRING (lab, str)) g =
-      ((Temp.name lab) ++ ":\n\t.asciz\t\"" ++ str ++ "\"\n", g)
-    step2 :: (String, Temp.Generator) -> X64Frame.Frag -> (String, Temp.Generator)
-    step2 (s, g) f =
-      let
-        (s', g') = emit f g
-      in
-        (s ++ s', g')
-  in
-    fst $ foldl' step2 ([], gen) frags
+            fst $ foldl' step2 ([], gen) frags
 
 main :: IO ()
 main = do
