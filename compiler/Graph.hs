99d59799f3e8abe65fdde19089ffaf52dc58e003
Tom Peters <thomas.d.peters@gmail.com>
2019-11-22 00:24:39 -0500

    filling out graph.hs

--- compiler/Graph.hs
+++ compiler/Graph.hs
@@ -1,20 +1,16 @@
 module Graph where
 
+import Data.List
 import Data.Map (Map)
 import qualified Data.Map as Map
 
 import Prelude hiding (succ, pred)
 
 
-data InfList a = InfList a (InfList a)
-
-genInfList :: (a -> a) -> a -> InfList a
-genInfList f elt = InfList elt (genInfList f $ f elt)
-
 type NodeId = Int
 
-data Node = Node { succ :: [Node]
-                 , pred :: [Node]
+data Node = Node { succ :: [NodeId]
+                 , pred :: [NodeId]
                  , nodeId :: NodeId }
 
 freshNode :: NodeId -> Node
@@ -23,23 +19,43 @@ freshNode nId = Node { succ=[]
                      , nodeId=nId }
 
 data Graph = Graph { nodes :: Map Int Node
-                   , _idSupply :: InfList Int }
+                   , nextId :: NodeId }
 
 newGraph :: Graph
 newGraph = Graph { nodes=Map.empty
-                 , _idSupply=genInfList (+1) 0 }
+                 , nextId=0 }
 
 newNode :: Graph -> (Node, Graph)
-newNode g@(Graph{_idSupply=(InfList nId nIds@(InfList _ _))}) =
+newNode g@(Graph{nextId=nId}) =
   let
     node = freshNode nId
   in
     ( node
     , g{ nodes=Map.insert nId node $ nodes g
-       , _idSupply=nIds } )
+       , nextId=nId + 1 } )
 
-mkEdge :: Graph -> Int -> Int -> Graph
-mkEdge = undefined
+mkEdge :: Graph -> NodeId -> NodeId -> Graph
+mkEdge g id1 id2 =
+  let
+    nodes_g = nodes g
+    n1 = nodes_g Map.! id1
+    n2 = nodes_g Map.! id2
+    n1' = n1{succ=[id2] ++ succ n1}
+    n2' = n2{pred=[id2] ++ pred n2}
+    nodes_g' = Map.insert id1 n1' nodes_g
+    nodes_g'' = Map.insert id2 n2' nodes_g'
+  in
+    g{nodes=nodes_g''}
 
-rmEdge :: Graph -> Int -> Int -> Graph
-rmEdge = undefined
+rmEdge :: Graph -> NodeId -> NodeId -> Graph
+rmEdge g id1 id2 =
+  let
+    nodes_g = nodes g
+    n1 = nodes_g Map.! id1
+    n2 = nodes_g Map.! id2
+    n1' = n1{succ=delete id2 $ succ n1}
+    n2' = n2{pred=delete id1 $ pred n2}
+    nodes_g' = Map.insert id1 n1' nodes_g
+    nodes_g'' = Map.insert id2 n2' nodes_g'
+  in
+    g{nodes=nodes_g''}
