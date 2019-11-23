127f63af607b9077f702824faabd804a60a45744
Tom Peters <thomas.d.peters@gmail.com>
2019-11-23 00:27:57 -0500

    filling out buildCFG

--- compiler/Flow.hs
+++ compiler/Flow.hs
@@ -3,6 +3,7 @@ module Flow where
 import qualified Assem as A
 import qualified Graph as G
 
+import Data.List
 import Data.Map (Map)
 import qualified Data.Map as Map
 import Control.Monad.Trans.State (runState)
@@ -18,27 +19,49 @@ data FlowGraph = FlowGraph { control :: G.Graph
 instrsToGraph :: [A.Inst] -> (FlowGraph, [G.Node])
 instrsToGraph insts =
   let
-    ((), g) = runState (buildGraph insts) G.newGraph
+    ((), g) = runState buildGraph G.newGraph
   in
     undefined
   where
-    buildGraph :: [A.Inst] -> G.GraphBuilder ()
+    buildGraph :: G.GraphBuilder ()
     buildGraph = undefined
 
-    allocNodes :: [A.Inst] -> G.GraphBuilder [(A.Inst, G.Node)]
-    allocNodes insts = mapM
-                         (\inst -> do
-                                     node <- G.allocNode
-                                     pure (inst, node))
-                         insts
+    allocNodes :: G.GraphBuilder [(A.Inst, G.Node)]
+    allocNodes = mapM
+                   (\inst -> do
+                               node <- G.allocNode
+                               pure (inst, node))
+                   insts
 
-    buildCFG :: [A.Inst] -> G.GraphBuilder ()
-    buildCFG insts = do
-     nodes <- allocNodes insts
-     mapM_
-       (\((i1, n1), (i2, n2)) ->
+    buildCFG :: G.GraphBuilder ()
+    buildCFG = do
+     nodes <- allocNodes
+     mapM_ -- compute fallthroughs
+       (\((i1, n1), (_, n2)) ->
           case i1 of
-            A.OPER { A.jump=Just targets } -> undefined
+            A.OPER { A.jump=Just _ } -> do pure ()
             _ -> G.addEdge n1 n2
         )
-       (zip nodes $ tail nodes)
+       $ zip nodes $ tail nodes
+     mapM_ -- compute explicit jumps
+       (\(i, n) ->
+          case i of
+            A.OPER { A.jump = Just jumpTargetLabs } ->
+              let
+                jumpTargetNodes = fmap
+                                    (\lab -> find
+                                               (\(inst, _) -> case inst of
+                                                    A.LABEL { A.lab=lab' } -> lab == lab'
+                                                    _ -> False)
+                                               nodes)
+                                    jumpTargetLabs
+                jumpTargetNodes' = case sequence jumpTargetNodes of
+                                     Just targets -> fmap snd targets
+                                     _ -> error "invalid instruction list"
+              in do
+                mapM_
+                  (G.addEdge n)
+                  jumpTargetNodes'
+            _ -> do pure ()
+        )
+        nodes
