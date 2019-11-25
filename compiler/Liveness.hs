1f9b129307daaf26ef7667ec83783ec0d3871b0a
Tom Peters <thomas.d.peters@gmail.com>
2019-11-25 00:32:11 -0500

    computing live sets

--- compiler/Liveness.hs
+++ compiler/Liveness.hs
@@ -4,7 +4,6 @@ import qualified Assem as A
 import qualified Graph as G
 import qualified Flow as Flow
 
-import Control.Monad.Trans.State (runState)
 import Data.List
 import Data.Map (Map)
 import qualified Data.Map as Map
@@ -37,5 +36,64 @@ data IGraph = IGraph { graph :: Graph
 interferenceGraph :: Flow.Graph -> (IGraph, Flow.Node -> [TempId])
 interferenceGraph = undefined
 
-buildLiveMap :: Flow.Graph -> Map Flow.NodeId (Set TempId)
-buildLiveMap = undefined
+-- | computes live-out sets per FlowGraph node
+buildLiveMap :: Flow.FlowGraph -> Map Flow.NodeId (Set TempId)
+buildLiveMap g =
+  let
+    flowKeys = Map.keys $ G.nodes $ Flow.control g
+    liveIn = Map.fromList $
+               fmap
+                 (\nodeId -> (nodeId, Set.empty))
+                 flowKeys
+    liveOut = liveIn
+  in
+    buildImpl flowKeys liveIn liveOut
+  where
+    buildImpl :: [Flow.NodeId]
+                 -> (Map Flow.NodeId (Set TempId))
+                 -> (Map Flow.NodeId (Set TempId))
+                 -> (Map Flow.NodeId (Set TempId))
+    buildImpl flowKeys liveIn liveOut =
+      let
+        liveIn'  = Map.fromList $
+                     fmap (updateIn liveOut) flowKeys
+        liveOut' = Map.fromList $
+                     fmap (updateOut liveIn) flowKeys
+        shouldStop = liveIn' == liveIn && liveOut' == liveOut
+      in
+        if shouldStop then
+          liveOut
+        else
+          buildImpl flowKeys liveIn' liveOut'
+
+    updateIn :: Map Flow.NodeId (Set TempId)
+                -> Flow.NodeId
+                -> (Flow.NodeId, Set TempId)
+    updateIn liveOut nodeId =
+      -- in[n] = use[n] \union (out[n]-def[n])
+      let
+        usesList = (Flow.use g) Map.! nodeId
+        usesSet = Set.fromList usesList
+        out = liveOut Map.! nodeId
+        defList = (Flow.def g) Map.! nodeId
+        defSet = Set.fromList defList
+      in
+        ( nodeId
+        , Set.union usesSet $ Set.difference out defSet )
+
+    updateOut :: Map Flow.NodeId (Set TempId)
+                -> Flow.NodeId
+                -> (Flow.NodeId, Set TempId)
+    updateOut liveIn nodeId =
+      -- out[n] = \Union_{s \in succ[n]} in[s]
+      let
+        succs = G.succ $ (G.nodes (Flow.control g)) Map.! nodeId
+        inOfSuccs = fmap
+                      (\succId -> liveIn Map.! succId)
+                      succs
+        out' = foldl'
+                 Set.union
+                 Set.empty
+                 inOfSuccs
+      in
+        (nodeId, out')
