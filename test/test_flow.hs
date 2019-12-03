1ec56f1688c7e1f905f3ebed4964018062e33934
Tom Peters <thomas.d.peters@gmail.com>
2019-12-02 23:32:15 -0500

    add some "asm" strings to test_flow

--- test/test_flow.hs
+++ test/test_flow.hs
@@ -15,29 +15,29 @@ main = hspec $ do
         c = 3
         l1 = Temp.Label $ S.Symbol ".L1"
         l2 = Temp.Label $ S.Symbol ".L2"
-        insts = [ A.OPER { A.assem=""
+        insts = [ A.OPER { A.assem="a <- 0"
                          , A.operDst=[a]
                          , A.operSrc=[]
                          , A.jump=Nothing }
-                , A.LABEL { A.assem=""
+                , A.LABEL { A.assem=".L1"
                           , A.lab=l2 }
-                , A.OPER { A.assem=""
+                , A.OPER { A.assem="b <- a + 1"
                          , A.operDst=[b]
                          , A.operSrc=[a]
                          , A.jump=Nothing }
-                , A.OPER { A.assem=""
+                , A.OPER { A.assem="c <- c + b"
                          , A.operDst=[c]
                          , A.operSrc=[c,b]
                          , A.jump=Nothing }
-                , A.OPER { A.assem=""
+                , A.OPER { A.assem="a <- b * 2"
                          , A.operDst=[a]
                          , A.operSrc=[b]
                          , A.jump=Nothing }
-                , A.OPER { A.assem=""
+                , A.OPER { A.assem="if a < N goto .L1"
                          , A.operDst=[]
                          , A.operSrc=[a]
                          , A.jump=Just [l1, l2] }
-                , A.LABEL { A.assem=""
+                , A.LABEL { A.assem="return c"
                           , A.lab=l1 }
                 ]
         (g, nodes) = F.instrsToGraph insts
