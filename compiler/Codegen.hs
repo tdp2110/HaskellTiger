6e2bd79f06469931fffaac68e9c726f65f37d828
Tom Peters <thomas.d.peters@gmail.com>
2020-04-12 11:37:48 -0400

    insert a dead store for Call codegen w/o result

--- compiler/Codegen.hs
+++ compiler/Codegen.hs
@@ -314,7 +314,10 @@ munchExp (TreeIR.CALL (expr, args, escapes, hasRet)) =
                                        , A.moveDst=r
                                        , A.moveSrc=X64Frame.rax x64 })
                       else
-                        Nothing)
+                        (Just $ A.OPER { A.assem="\tmov `d0, 0xDEADBEEF"
+                                       , A.operDst=[r]
+                                       , A.operSrc=[]
+                                       , A.jump=Nothing }))
                      argRegs
                      escapes
                      IsReturn
