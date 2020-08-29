57c19925ad43768fd7bc69ed0fd7dc10671ed669
Tom Peters <thomas.d.peters@gmail.com>
2020-08-29 00:13:35 -0400

    trim newline in tiger_getchar

--- runtime/src/tiger-rt.cpp
+++ runtime/src/tiger-rt.cpp
@@ -47,7 +47,9 @@ uint8_t *tiger_alloc(int64_t const numBytes) {
 void tiger_flush() { std::cout << std::flush; }
 
 TgString *tiger_getchar() {
-  int const c = std::getchar();
+  char c;
+  std::cin >> c;
+  std::cin.get();
   TgString *const res = new (std::nothrow) TgString(std::string(1, c));
   AssertNotNull(res, __FUNCTION__);
   return res;
