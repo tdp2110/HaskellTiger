156b0cb0641ed45154dee55da74561624c192b4e
Tom Peters <thomas.d.peters@gmail.com>
2020-11-22 00:26:04 -0500

    varExp, opExp

--- compiler/LLVMCodegen.hs
+++ compiler/LLVMCodegen.hs
@@ -44,23 +44,39 @@ type Codegen = L.IRBuilderT LLVM
 
 codegenExp :: A.Exp -> Codegen AST.Operand
 codegenExp (A.IntExp i) = pure $ L.int64 $ toInteger i
+codegenExp (A.VarExp (A.SimpleVar sym pos)) = do
+  operandEnv <- gets operands
+  case M.lookup (S.name sym) operandEnv of
+    Just op -> L.load op 8
+    Nothing -> error $ "use of undefined variable "  <> show sym <> " at " <> show pos
+codegenExp (A.OpExp left oper right _) = do
+  leftOperand <- codegenExp left
+  rightOperand <- codegenExp right
+  let f = case oper of
+        A.PlusOp   -> L.add
+        A.MinusOp  -> L.sub
+        A.TimesOp  -> L.mul
+        A.DivideOp -> L.sdiv
+        _          -> error $ "unsupported operand " <> show oper
+  f leftOperand rightOperand
+codegenExp e = error $ "unimplemented alternative in codegenExp: " <> show e
 
 emptyModule :: String -> AST.Module
 emptyModule label = AST.defaultModule { AST.moduleName = toShortBS label }
 
 codegenTop :: A.Exp -> LLVM ()
 codegenTop (A.LetExp decs _ _) = do
-  _ <- forM decs cgenDecl
+  _ <- forM decs codegenDecl
   pure ()
 
 codegenTop e = error $ "implemented alternative in codegenTop: " <> show e
 
-cgenDecl :: A.Dec -> LLVM ()
-cgenDecl (A.FunctionDec [funDec]) = cgenFunDec funDec
-cgenDecl _                        = undefined
+codegenDecl :: A.Dec -> LLVM ()
+codegenDecl (A.FunctionDec [funDec]) = codegenFunDec funDec
+codegenDecl _                        = undefined
 
-cgenFunDec :: A.FunDec -> LLVM ()
-cgenFunDec A.FunDec { A.fundecName = name, A.params = params, A.funBody = body }
+codegenFunDec :: A.FunDec -> LLVM ()
+codegenFunDec A.FunDec { A.fundecName = name, A.params = params, A.funBody = body }
   = do
     fun <- L.function (AST.Name $ toShortBS $ show name) args AST.i64 genBody
     registerOperand (S.name name) fun
@@ -78,10 +94,11 @@ cgenFunDec A.FunDec { A.fundecName = name, A.params = params, A.funBody = body }
       addr <- L.alloca AST.i64 Nothing 8
       L.store addr 8 op
       registerOperand (S.name $ A.fieldName field) addr
+    codegenExp body >>= L.ret
 
 codegenLLVM :: A.Exp -> AST.Module
 codegenLLVM e =
-  let module' = emptyModule "llvm-test"
+  let
       res =
           flip evalState (Env { operands = M.empty })
             $ L.buildModuleT "llvm-test"
