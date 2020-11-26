8ef5a32e6607d21572cfda1a183b5a590e3ca5db
Tom Peters <thomas.d.peters@gmail.com>
2020-11-25 23:20:21 -0500

    starting type checking in LLVMCodegen

--- compiler/LLVMCodegen.hs
+++ compiler/LLVMCodegen.hs
@@ -6,6 +6,7 @@ module LLVMCodegen where
 
 import qualified Absyn                         as A
 import qualified Symbol                        as S
+import qualified Types                         as Types
 
 import qualified LLVM.AST                      as LL
 import qualified LLVM.AST.Type                 as LL
@@ -19,8 +20,9 @@ import qualified LLVM.IRBuilder.Constant       as IRB
 import           Data.Word
 import           Data.ByteString.Short
 import qualified Data.Map                      as M
+import           Control.Monad                  ( when )
 import           Control.Monad.State
-import qualified Data.Text                     as T
+import qualified Data.Text                     as Text
 import           Data.Text                      ( Text )
 
 
@@ -30,12 +32,12 @@ charToWord8 = toEnum . fromEnum
 toShortBS :: String -> ShortByteString
 toShortBS s = Data.ByteString.Short.pack $ fmap charToWord8 s
 
-newtype Env = Env { operands :: M.Map Text LL.Operand }
+newtype Env = Env { operands :: M.Map Text (LL.Operand, Types.Ty) }
         deriving (Eq, Show)
 
-registerOperand :: MonadState Env m => Text -> LL.Operand -> m ()
-registerOperand name op =
-  modify $ \env -> env { operands = M.insert name op (operands env) }
+registerOperand :: MonadState Env m => Text -> Types.Ty -> LL.Operand -> m ()
+registerOperand name ty op =
+  modify $ \env -> env { operands = M.insert name (op, ty) (operands env) }
 
 type LLVM = IRB.ModuleBuilderT (State Env)
 type Codegen = IRB.IRBuilderT LLVM
@@ -43,20 +45,22 @@ type Codegen = IRB.IRBuilderT LLVM
 zero :: LL.Operand
 zero = IRB.int64 (0 :: Integer)
 
-codegenExp :: A.Exp -> Codegen LL.Operand
+codegenExp :: A.Exp -> Codegen (LL.Operand, Types.Ty)
 
-codegenExp (A.IntExp i                    ) = pure $ IRB.int64 $ toInteger i
+codegenExp (A.IntExp i) = pure (IRB.int64 $ toInteger i, Types.INT)
 
 codegenExp (A.VarExp (A.SimpleVar sym pos)) = do
   operandEnv <- gets operands
   case M.lookup (S.name sym) operandEnv of
-    Just op -> IRB.load op 8
+    Just (op, opTy) -> do
+      loadOp <- IRB.load op 8
+      pure (loadOp, opTy)
     Nothing ->
       error $ "use of undefined variable " <> show sym <> " at " <> show pos
 
-codegenExp (A.OpExp left oper right _) = do
-  leftOperand  <- codegenExp left
-  rightOperand <- codegenExp right
+codegenExp (A.OpExp left oper right pos) = do
+  (leftOperand , leftTy ) <- codegenExp left
+  (rightOperand, rightTy) <- codegenExp right
   let f = case oper of
         A.PlusOp   -> IRB.add
         A.MinusOp  -> IRB.sub
@@ -69,63 +73,110 @@ codegenExp (A.OpExp left oper right _) = do
         A.GtOp     -> IRB.icmp LL.SGT
         A.GeOp     -> IRB.icmp LL.SGE
         A.ModOp    -> IRB.srem -- TODO need to check for division by zero
-  f leftOperand rightOperand
+  if leftTy /= Types.INT
+    then
+      error
+      $  "invalid operand of type "
+      <> show leftTy
+      <> " in binary operator at "
+      <> show pos
+    else if rightTy /= Types.INT
+      then
+        error
+        $  "invalid operand of type "
+        <> show right
+        <> " in binary operator at "
+        <> show pos
+      else do
+        res <- f leftOperand rightOperand
+        pure (res, Types.INT)
 
-codegenExp (A.AssignExp (A.SimpleVar var _) rhs pos) = do
-  rhsOp      <- codegenExp rhs
-  operandEnv <- gets operands
-  case M.lookup (S.name var) operandEnv of
-    Just lhsOp -> do
-      IRB.store lhsOp 8 rhsOp
-      pure zero
-    Nothing ->
-      error $ "use of undefined variable " <> show var <> " at " <> show pos
+codegenExp (A.AssignExp (A.SimpleVar var varPos) rhs rhsPos) = do
+  (rhsOp, rhsTy) <- codegenExp rhs
+  if rhsTy == Types.UNIT
+    then error $ "cannot assign a expression of type UNIT at " <> show rhsPos
+    else do
+      operandEnv <- gets operands
+      case M.lookup (S.name var) operandEnv of
+        Just (lhsOp, _) -> do
+          IRB.store lhsOp 8 rhsOp
+          pure (zero, Types.UNIT)
+        Nothing ->
+          error
+            $  "use of undefined variable "
+            <> show var
+            <> " at "
+            <> show varPos
 
-codegenExp (A.IfExp test then' (Just else') _) = mdo
+codegenExp (A.IfExp test then' (Just else') pos) = mdo
   -- %entry
   ---------
-  testOp <- codegenExp test
-  test'  <- IRB.icmp LL.NE testOp zero
+  (testOp, testTy) <- codegenExp test
+  when (testTy /= Types.INT) $ do
+    error
+      $  "test expressions must be INT. Found "
+      <> show testTy
+      <> " at "
+      <> show pos
+
+  test' <- IRB.icmp LL.NE testOp zero
   IRB.condBr test' ifThen ifElse
 
   -- %if.then
   -----------
-  ifThen   <- IRB.block `IRB.named` "if.then"
-  ifThenOp <- codegenExp then'
+  ifThen               <- IRB.block `IRB.named` "if.then"
+  (ifThenOp, ifThenTy) <- codegenExp then'
   IRB.br ifExit
 
   -- %if.else
   -----------
-  ifElse   <- IRB.block `IRB.named` "if.else"
-  ifElseOp <- codegenExp else'
+  ifElse               <- IRB.block `IRB.named` "if.else"
+  (ifElseOp, ifElseTy) <- codegenExp else'
+  when (ifThenTy /= ifElseTy) $ do
+    error
+      $ "In if else expressions, both ifTrue and ifFalse branches must have the same type. Found "
+      <> show ifThenTy
+      <> " and "
+      <> show ifElseTy
+      <> " at "
+      <> show pos
   IRB.br ifExit
 
   -- %if.exit
   -----------
   ifExit <- IRB.block `IRB.named` "if.exit"
-  IRB.phi [(ifThenOp, ifThen), (ifElseOp, ifElse)]
+  phi    <- IRB.phi [(ifThenOp, ifThen), (ifElseOp, ifElse)]
+  pure (phi, ifElseTy)
 
 codegenExp (A.SeqExp expAndPosns) = do
-  exps <- forM expAndPosns (\(e, _) -> codegenExp e)
-  case exps of
-    [] -> pure zero
-    _  -> pure $ last exps
+  expsAndTys <- forM expAndPosns (\(e, _) -> codegenExp e)
+  case expsAndTys of
+    [] -> pure (zero, Types.UNIT)
+    _  -> pure $ last expsAndTys
 
 codegenExp (A.CallExp funcSym args _) = do
   argOps <- forM args $ \arg -> do
-    argOp <- codegenExp arg
+    (argOp, _) <- codegenExp arg -- TODO type check
     pure (argOp, [])
   operandEnv <- gets operands
   case M.lookup (S.name funcSym) operandEnv of
-    Just funcOp -> IRB.call funcOp argOps
-    Nothing     -> error $ "use of undeclared function " <> show funcSym
+    Just (funcOp, _) -> do
+      call <- IRB.call funcOp argOps
+      pure (call, Types.INT) -- TODO not always int!
+    Nothing -> error $ "use of undeclared function " <> show funcSym
 
-codegenExp (A.WhileExp test body _) = mdo
+codegenExp (A.WhileExp test body pos) = mdo
   IRB.br testLab
 
-  testLab <- IRB.block `IRB.named` "while.test"
-  testOp  <- codegenExp test
-  test'   <- IRB.icmp LL.NE testOp zero
+  testLab          <- IRB.block `IRB.named` "while.test"
+  (testOp, testTy) <- codegenExp test
+  when (testTy /= Types.INT) $ do
+    error
+      $  "while test conditions must be INT. Found "
+      <> show testTy
+      <> " at "
+      <> show pos
+  test' <- IRB.icmp LL.NE testOp zero
   IRB.condBr test' loopBody loopExit
 
   loopBody <- IRB.block `IRB.named` "while.body"
@@ -133,7 +184,7 @@ codegenExp (A.WhileExp test body _) = mdo
   IRB.br testLab
 
   loopExit <- IRB.block `IRB.named` "while.exit"
-  pure zero
+  pure (zero, Types.UNIT)
 
 codegenExp (A.LetExp decs body _) = do
   forM_ decs codegenDecl
@@ -159,17 +210,17 @@ codegenTop e =
 codegenDecl :: A.Dec -> Codegen ()
 codegenDecl (A.FunctionDec [funDec]     ) = lift $ codegenFunDec funDec
 codegenDecl (A.VarDec name _ _ initExp _) = do
-  initOp <- codegenExp initExp
-  addr   <- IRB.alloca LL.i64 Nothing 8
+  (initOp, initTy) <- codegenExp initExp
+  addr             <- IRB.alloca LL.i64 Nothing 8
   IRB.store addr 8 initOp
-  registerOperand (S.name name) addr
+  registerOperand (S.name name) initTy addr
   pure ()
 codegenDecl _ = undefined
 
 codegenFunDec :: A.FunDec -> LLVM ()
 codegenFunDec A.FunDec { A.fundecName = name, A.params = params, A.funBody = body }
   = mdo
-    registerOperand (S.name name) fun
+    registerOperand (S.name name) Types.UNIT fun -- TODO fixup function type!
     fun <- IRB.function (LL.Name $ toShortBS $ show name) args LL.i64 genBody
     pure ()
  where
@@ -185,8 +236,9 @@ codegenFunDec A.FunDec { A.fundecName = name, A.params = params, A.funBody = bod
     forM_ (zip ops params) $ \(op, field) -> do
       addr <- IRB.alloca LL.i64 Nothing 8
       IRB.store addr 8 op
-      registerOperand (S.name $ A.fieldName field) addr
-    codegenExp body >>= IRB.ret
+      registerOperand (S.name $ A.fieldName field) Types.INT addr -- TODO fixup param types
+    (bodyOp, _) <- codegenExp body
+    IRB.ret bodyOp
 
 newtype InternalName = InternalName String
 newtype ExternalName = ExternalName String
@@ -204,7 +256,7 @@ emitBuiltin :: (InternalName, ExternalName, [LL.Type], LL.Type) -> LLVM ()
 emitBuiltin (InternalName internalName, ExternalName externalName, argtys, retty)
   = do
     func <- IRB.extern (LL.mkName externalName) argtys retty
-    registerOperand (T.pack internalName) func
+    registerOperand (Text.pack internalName) Types.UNIT func -- fixup function types
 
 codegenLLVM :: A.Exp -> LL.Module
 codegenLLVM e =
